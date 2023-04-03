e43ea82e060d9e08984360ac0e1c87b8ee8790ed
Ram Iyer <ramky.iyer@gmail.com>
2023-04-03 15:05:53 -0400

    adding outlet with react-router

--- go-movies-front-end/src/index.js
+++ go-movies-front-end/src/index.js
@@ -4,11 +4,18 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import ErrorPage from './components/ErrorPage'
+import Home from './components/Home'
+import Movies from './components/Movies'
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/movies", element: <Movies />}
+    ]
   }
 ])
 
