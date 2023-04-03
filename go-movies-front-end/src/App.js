51363ea90209886ec7c1d3353ee4ede77736bf48
Ram Iyer <ramky.iyer@gmail.com>
2023-04-03 14:47:18 -0400

    starting with basic front-end app with home page nav links

--- go-movies-front-end/src/App.js
+++ go-movies-front-end/src/App.js
@@ -1,8 +1,35 @@
+import Home from './components/Home'
 
 function App() {
   return (
-    <div>
-      Go watch a movie!
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go watch a movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!"><span className="badge bg-success">Login</span></a>
+        </div>
+        <hr className="md-3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">Home</a>
+              <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+              <a href="#!" className="list-group-item list-group-item-action">Genres</a>
+              <a href="#!" className="list-group-item list-group-item-action">Add Movie</a>
+              <a href="#!" className="list-group-item list-group-item-action">Manage Catalog</a>
+              <a href="#!" className="list-group-item list-group-item-action">GraphQL</a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+          <Home />
+        </div>
+      </div>
     </div>
   );
 }
